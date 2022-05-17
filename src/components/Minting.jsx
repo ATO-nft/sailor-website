e86ad7a65f3ff7408d110745527b483ea3c1415f
julienbrg <julien@strat.cc>
2022-05-17 16:48:06 +0200

    add minting

--- src/components/Minting.jsx
+++ src/components/Minting.jsx
@@ -3,7 +3,13 @@ import { PlasmicMinting } from "./plasmic/sailor_alpha/PlasmicMinting";
 
 function Minting_(props, ref) {
   const { plasmicProps } = PlasmicMinting.useBehavior(props, ref);
-  return <PlasmicMinting {...plasmicProps} />;
+  return <PlasmicMinting 
+    root={{
+      props: { ...plasmicProps,
+      onClick: () => handleMintAgain()
+      }
+    }}
+    />;
 }
 
 const Minting = React.forwardRef(Minting_);
@@ -11,3 +17,8 @@ const Minting = React.forwardRef(Minting_);
 export default Object.assign(Minting, {
   __plumeType: "button"
 });
+
+// Overrides
+async function handleMintAgain() {
+  console.log("You win AGAIN!")
+}
