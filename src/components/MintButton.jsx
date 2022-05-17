e6b97874f443c6c9af1290d057719ed972c39e91
julienbrg <julien@strat.cc>
2022-05-17 17:35:10 +0200

    rename to MintButton

--- src/components/MintButton.jsx
+++ src/components/MintButton.jsx
@@ -3,7 +3,13 @@ import { PlasmicMintButton } from "./plasmic/sailor_alpha/PlasmicMintButton";
 
 function MintButton_(props, ref) {
   const { plasmicProps } = PlasmicMintButton.useBehavior(props, ref);
-  return <PlasmicMintButton {...plasmicProps} />;
+  return <PlasmicMintButton
+  root={{
+    props: { ...plasmicProps,
+    onClick: () => handleMintAgain()
+    }
+  }}
+  />;
 }
 
 const MintButton = React.forwardRef(MintButton_);
@@ -11,3 +17,8 @@ const MintButton = React.forwardRef(MintButton_);
 export default Object.assign(MintButton, {
   __plumeType: "button"
 });
+
+// Overrides
+async function handleMintAgain() {
+  console.log("Starting the minting process...")
+}
