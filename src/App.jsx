20529f6708d348024d7038cc3cfca3ec77e211d3
julienbrg <julien@strat.cc>
2022-05-17 17:00:30 +0200

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./components/Homepage";
 import Mint from "./components/Mint";
 import Artwork from "./components/Artwork";
+import Roadmap from "./components/Roadmap";
 
 function Index() {
   return (
@@ -11,6 +12,7 @@ function Index() {
         <Route path="/" element={<Homepage />} />
         <Route path="/mint" element={<Mint />} />
         <Route path="/artwork" element={<Artwork />} />
+        <Route path="/roadmap" element={<Roadmap />} />
       </Routes>
     </Router>
   );
