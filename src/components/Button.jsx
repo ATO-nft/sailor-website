1b37891f29694659a06c2bdeb4cf55781158038c
julienbrg <julien@strat.cc>
2022-05-17 16:27:21 +0200

    add handleMint

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,13 @@ import { PlasmicButton } from "./plasmic/sailor_alpha/PlasmicButton";
 
 function Button_(props, ref) {
   const { plasmicProps } = PlasmicButton.useBehavior(props, ref);
-  return <PlasmicButton {...plasmicProps} />;
+  return <PlasmicButton 
+    root={{
+      props: { ...plasmicProps,
+      onClick: () => handleMint()
+      }
+    }}
+    />;
 }
 
 const Button = React.forwardRef(Button_);
@@ -11,3 +17,9 @@ const Button = React.forwardRef(Button_);
 export default Object.assign(Button, {
   __plumeType: "button"
 });
+
+// Overrides
+async function handleMint() {
+  console.log("You win!")
+}
+
