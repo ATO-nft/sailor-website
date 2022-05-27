b3ba8c960b3db3a0fd577e06befecdb6433fa21c
julienbrg <julien@strat.cc>
2022-05-27 14:52:52 +0200

    add Artwork2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Artwork from "./components/Artwork";
 import Roadmap from "./components/Roadmap";
 import Gallery from "./components/Gallery";
 import Artist from "./components/Artist";
+import Artwork2 from "./components/Artwork2";
+
 
 function Index() {
   return (
@@ -17,6 +19,7 @@ function Index() {
         <Route path="/roadmap" element={<Roadmap />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/artist" element={<Artist />} />
+        <Route path="/artwork-2" element={<Artwork2 />} />
       </Routes>
     </Router>
   );
