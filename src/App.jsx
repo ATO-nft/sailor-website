aa1a978c60353f8c9ee72968f8e3597c6fe41db3
julienbrg <julien@strat.cc>
2022-06-01 18:33:02 +0200

    add vision route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Artwork from "./components/Artwork";
 import Roadmap from "./components/Roadmap";
 import Gallery from "./components/Gallery";
 import Artist from "./components/Artist";
-import Artwork2 from "./components/Artwork2";
+import Vision from "./components/Vision";
 
 
 function Index() {
@@ -19,7 +19,7 @@ function Index() {
         <Route path="/roadmap" element={<Roadmap />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/artist" element={<Artist />} />
-        <Route path="/artwork-2" element={<Artwork2 />} />
+        <Route path="/vision" element={<Vision />} />
       </Routes>
     </Router>
   );
